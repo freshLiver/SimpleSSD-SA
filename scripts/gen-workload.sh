e7a9eecda255b833ff4e00b1f0060d2fae7e3414
Yuanhsi Chung <freshliver.cys@gmail.com>
2024-08-03 16:12:49 +0800

    Scripts: gen workload without given beg/end tick

--- scripts/gen-workload.sh
+++ scripts/gen-workload.sh
@@ -1,32 +1,44 @@
 #!/bin/bash
 
-BegTick=$2
-EndTick=$3
+gem5LogFile=$1
+hostLogFile=$2
 
-if [[ "$#" -ne 3 ]]; then
-    echo "Usage: $0 LogFile BegTick EndTick"
+if [[ "$#" -ne 2 ]]; then
+    echo "Usage: $0 gem5LogFile hostLogFile"
     exit -1
 fi
 
-# Sample: "RECORD: 24.606908705795 R 0x000000011938 + 0x8 [B64 DATA]"
+# Since this script needs 2 arguments, it's not easy to generate workloads from
+# multiple logs, you can write another script to call this script in loop
+#
+# Note that the hostLogFile must contain the information of begin/end tick in
+# the format: 'Simulation Time: \d+~\d+ \(\d+\) ps'
 
-# remove leading hints and trailing spaces
-records="$(grep "RECORD" $1 | sed -E 's/^RECORD: //g' | sed -E 's/ +$//g')"
+if [[ ! -e "$1" ]]; then
+    echo "The specified gem5LogFile '$1' not exists"
+    exit -1
+fi
+if [[ ! -e "$2" ]]; then
+    echo "The specified hostLogFile '$2' not exists"
+    exit -1
+fi
 
-# extract time formats
-echo "$records" | sed -E 's/([0-9]+)\.([0-9]{12})/\1\2/g' | awk "
-BEGIN {
-    S = $BegTick
-    E = $EndTick
-    CNT = 0
-}
+# extract beg/end tick from host log
+psTimeInfo="$(grep -oP 'Simulation Time: \d+~\d+ \(\d+\) ps' $2)"
+read psBegTick psEndTick <<< "$(grep -oP "\d+~\d+" <<< $psTimeInfo | tr '~' ' ')"
+sBegTick=$(bc <<< "scale=12; $psBegTick / 10^12")
+sEndTick=$(bc <<< "scale=12; $psEndTick / 10^12")
 
-S <= \$1 && \$1 <= E {
-    print
+# extract records within target tick range from gem5LogFile
+grep -P "^RECORD: \d+\.\d+" "$1" | awk -v S="$sBegTick" -v E="$sEndTick" '
+BEGIN { CNT = 0 }
+
+S <= $2 && $2 <= E {
+    for (i = 2; i < NF; i++)
+        printf "%s ", $i
+    print $NF
     CNT += 1
 }
 
-END {
-    printf \"# %d records are extracted\n\", CNT
-}
-"
+END { printf "# %d records are extracted\n", CNT }
+'
